b7eaba7f142cb85ab989d08658f8434450d7a75d
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-02 16:44:00 -0400

    docs: add commenting to code

--- database/utils/db.py
+++ database/utils/db.py
@@ -10,9 +10,13 @@ DB_NAME = getenv("DB")
 mongo_client = MongoClient(MONGO_URI)
 
 def open_connection() -> MongoClient:
+    """Open connection to mongo database
+    """
     db = mongo_client.get_database(DB_NAME)
     return db
 
 def close_connection() -> None:
+    """Close connection to mongo database
+    """
     mongo_client.close()
 
