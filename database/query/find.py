dd83c8dbae568993f2dd0e82ab7687cb1e3166d8
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-07 11:54:46 -0400

    fix: change imports

--- database/query/find.py
+++ database/query/find.py
@@ -1,26 +1,45 @@
 from utils.db import open_connection, close_connection
-from constant.collection import Collection
+from common.constant.collection import Collection
+from models.node import Node
 
-def find_node(state: str, central: str) -> dict:
+def find_node(state: str, central: str) -> Node:
     try:
         db = open_connection()
-        collection = db.get_collection(Collection.node)
+        collection = db.get_collection(Collection.NODE)
         node = collection.find_one({"state": state, "central": central})
+        if node: res = Node(**node)
+        else: res = None
     except Exception as error:
         close_connection()
         raise error
     else:
         close_connection()
-        return node
+        return res
     
-def find_node_by_ip(ip: str) -> dict:
+def find_node_by_ip(ip: str) -> Node:
     try:
         db = open_connection()
-        collection = db.get_collection(Collection.node)
+        collection = db.get_collection(Collection.NODE)
         node = collection.find_one({"ip": ip})
+        if node: res = Node(**node)
+        else: res = None
     except Exception as error:
         close_connection()
         raise error
     else:
         close_connection()
-        return node
+        return res
+
+def find_node_by_central(central: str) -> Node:
+    try:
+        db = open_connection()
+        collection = db.get_collection(Collection.NODE)
+        node = collection.find_one({"central": central})
+        if node: res = Node(**node)
+        else: res = None
+    except Exception as error:
+        close_connection()
+        raise error
+    else:
+        close_connection()
+        return res
--- database/query/insert.py
+++ database/query/insert.py
@@ -1,12 +1,12 @@
 from utils.db import open_connection, close_connection
 from entity.node import Node
-from constant.collection import Collection
+from common.constant.collection import Collection
 from pymongo.results import InsertOneResult, InsertManyResult
 
 def insert_new_node(node: Node) -> InsertOneResult:
     try:
         db = open_connection()
-        collection = db.get_collection(Collection.node)
+        collection = db.get_collection(Collection.NODE)
         res = collection.insert_one(node.model_dump())
     except Exception as error:
         close_connection()
@@ -18,7 +18,7 @@ def insert_new_node(node: Node) -> InsertOneResult:
 def insert_new_nodes(nodes: list[Node]) -> InsertManyResult:
     try:
         db = open_connection()
-        collection = db.get_collection(Collection.node)
+        collection = db.get_collection(Collection.NODE)
         nodes_list = [node.model_dump() for node in nodes]
         res = collection.insert_many(nodes_list)
     except Exception as error:
