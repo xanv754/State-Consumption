daddd6e123a7f91dd5965dac06bad5a4905132a2
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-09 09:57:19 -0400

    feat: add fix«Á»

--- common/utils/fix.py
+++ common/utils/fix.py
@@ -4,15 +4,21 @@ def fix_column_word(df: pd.DataFrame, column_name: str):
     try:
         # ß -> á
         df[column_name] = df[column_name].str.replace('ß', 'a')
+        # ┴ -> Á
+        df[column_name] = df[column_name].str.replace('┴', 'A')
         # Ú -> é
         df[column_name] = df[column_name].str.replace('Ú', 'é')
+        # TODO: ... -> É
         # Ý -> í
         df[column_name] = df[column_name].str.replace('Ý', 'í')
+        # TODO: ... -> Í
         # =, ¾ -> ó
         df[column_name] = df[column_name].str.replace('=', 'ó')
         df[column_name] = df[column_name].str.replace('¾', 'ó')
+        # TODO: ... -> Ó
         # · -> ú
         df[column_name] = df[column_name].str.replace('·', 'ú')
+        # TODO: ... -> Ú
         # ± -> ñ
         df[column_name] = df[column_name].str.replace('±', 'ñ')
         # &#209 -> Ñ
