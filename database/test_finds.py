ccb0d4b2b8de25da7c9391f1f237b116123c623c
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-10 13:46:44 -0400

    refactor: update function name

--- database/test_finds.py
+++ database/test_finds.py
@@ -1,11 +1,11 @@
-from query.find import find_node, find_node_by_ip
+from query.find import find_node_by_state, find_node_by_ip
 
 STATE = "Distrito Capital"
 CENTRAL = "Carapita"
 IP = "127.0.0.1"
 
 def test_find_node():
-    res = find_node(STATE, CENTRAL)
+    res = find_node_by_state(STATE, CENTRAL)
     assert res != None
 
 def test_find_node_by_ip():
