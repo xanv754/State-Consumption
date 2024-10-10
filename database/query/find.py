2badef27799fee31dfe644cc4f9090cdb9f9779f
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-10 13:46:25 -0400

    refactor: change function name & change variable position name

--- database/query/find.py
+++ database/query/find.py
@@ -2,7 +2,7 @@ from utils.db import open_connection, close_connection
 from common.constant.collection import Collection
 from models.node import Node
 
-def find_node(state: str, central: str) -> Node:
+def find_node_by_state(state: str, central: str) -> Node:
     try:
         db = open_connection()
         collection = db.get_collection(Collection.NODE)
@@ -30,7 +30,7 @@ def find_node_by_ip(ip: str) -> Node:
         close_connection()
         return res
 
-def find_node_by_central(central: str, account_code: str) -> Node:
+def find_node_by_account_code(account_code: str, central: str) -> Node:
     try:
         db = open_connection()
         collection = db.get_collection(Collection.NODE)
