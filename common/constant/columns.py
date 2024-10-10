d5345a5d714e1234c8e27fd260cf8449d606c9f8
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-10 12:41:30 -0400

    feat: add new columns

--- common/constant/columns.py
+++ common/constant/columns.py
@@ -4,3 +4,6 @@ class ColumnsReport:
     ACRONYM_BRAS = "Location"
     BRAS = "Nrpname"
     CLIENTS = "Cantidad"
+    STATE = "Estado"
+    TOTAL_BY_BRAS = "Total por Agregador"
+    TOTAL_BY_STATE = "Total por Estado"
