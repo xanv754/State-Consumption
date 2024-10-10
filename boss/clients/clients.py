ced75a050d0b97c143dfc04d60de9d2f2ff94e19
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-10 13:47:33 -0400

    refactor: update class name

--- boss/clients/clients.py
+++ boss/clients/clients.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from tqdm import tqdm
-from common.constant.columns import ColumnsReport
+from common.constant.columns import ReportColumns
 
 class TableController:
 
@@ -9,15 +9,15 @@ class TableController:
         try:
             #TODO: Separate ADSL and MDU
             tqdm.write("Create merge between BRAS and State Node...")
-            df[ColumnsReport.CLIENTS] = df[ColumnsReport.CLIENTS].astype(int)
+            df[ReportColumns.CLIENTS] = df[ReportColumns.CLIENTS].astype(int)
             df_bras = pd.DataFrame()
-            df = df.sort_values(by=ColumnsReport.STATE, ascending=True)
-            new_df = pd.DataFrame({ColumnsReport.STATE: list(df[ColumnsReport.STATE].unique())})
+            df = df.sort_values(by=ReportColumns.STATE, ascending=True)
+            new_df = pd.DataFrame({ReportColumns.STATE: list(df[ReportColumns.STATE].unique())})
             list_bras = list(df.sort_values(by="bras", ascending=True)["bras"].unique())
             for bras_name in tqdm(list_bras):
-                clients = df[df["bras"] == bras_name].groupby(ColumnsReport.STATE)[ColumnsReport.CLIENTS].sum()
-                df_merge = new_df.merge(clients, how="outer", on=[ColumnsReport.STATE], indicator=False)
-                df_bras[bras_name] = df_merge[ColumnsReport.CLIENTS]
+                clients = df[df["bras"] == bras_name].groupby(ReportColumns.STATE)[ReportColumns.CLIENTS].sum()
+                df_merge = new_df.merge(clients, how="outer", on=[ReportColumns.STATE], indicator=False)
+                df_bras[bras_name] = df_merge[ReportColumns.CLIENTS]
                 new_df[bras_name] = df_bras[bras_name]
             return new_df
         except Exception as error:
@@ -29,7 +29,7 @@ class TableController:
             tqdm.write("Add total col sum...")
             index_col = len(df)
             list_columns = df.columns.to_list()[1:]
-            df.loc[index_col, df.columns[0]] = ColumnsReport.TOTAL_BY_BRAS
+            df.loc[index_col, df.columns[0]] = ReportColumns.TOTAL_BY_BRAS
             for column_name in tqdm(list_columns): 
                 df.loc[index_col, column_name] = df[column_name].sum()
             return df
@@ -40,8 +40,8 @@ class TableController:
     def add_total_sum_by_state(df: pd.DataFrame) -> pd.DataFrame:
         try:
             tqdm.write("Add total row sum...")
-            df_filter = df.drop(ColumnsReport.STATE, axis=1)
-            df[ColumnsReport.TOTAL_BY_STATE] = df_filter.sum(axis=1)
+            df_filter = df.drop(ReportColumns.STATE, axis=1)
+            df[ReportColumns.TOTAL_BY_STATE] = df_filter.sum(axis=1)
             return df
         except Exception as error:
             raise error
