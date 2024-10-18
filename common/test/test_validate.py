c200bdd0619cdfff6d980f5f1c05db85d9a1081e
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-17 22:06:42 -0400

    test: add unit test for utils common module

---
+++ common/test/test_validate.py
@@ -0,0 +1,7 @@
+from common.utils.validate import validate_name_bras
+
+def test_validate_name_bras():
+    assert (validate_name_bras("ANZ-BRAS-00_HUAWEI_00") == True
+            and validate_name_bras("ANZ") == False
+            and validate_name_bras("PER-BRAS-00_HUAWEI_00-00") == False
+        )
