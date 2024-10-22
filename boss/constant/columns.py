056bce1e19c07efd14abc31404f09ab6a60ee8e9
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-22 14:54:10 -0400

    refactor: delete comments

--- boss/constant/columns.py
+++ boss/constant/columns.py
@@ -5,10 +5,3 @@ ACRONYM_BRAS = "Location"
 BRAS = "Nrpname"
 CLIENTS = "Cantidad"
 STATE = ["ESTADO", "Estado", "estado", "EDO", "Edo", "edo"]
-
-# # NEW BOSS
-# NEW_STATE = "Estado"
-# NEW_BRAS = "BRAS"
-# TOTAL_BY_BRAS = "Total por Agregador"
-# TOTAL_BY_STATE = "Total por Estado"
-# TOTAL_BY_USAGE = "Total de consumo por Estado"
