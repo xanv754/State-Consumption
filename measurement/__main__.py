3585ecd2c6215773146b86c3e9eda2337bc2a5b8
Angxandralol <angyeemarin.dev@gmail.com>
2024-10-30 13:00:36 -0400

    docs: add README.md & README_es.md to measurement module

--- measurement/__main__.py
+++ measurement/__main__.py
@@ -16,14 +16,11 @@ def taccess():
 @cli.command(help="Get consumption from external file.")
 @click.option("-f", "--file", type=click.Path(exists=True), help="File to be read.")
 @click.option("-s", "--sheetname", default="" , help="Specify sheetname of file to be read")
-@click.option("-p", "--process", help="Allows you to save all files generated in execution", is_flag=True)
-def external(file, sheetname, process):
-    if file and sheetname and process:
-        get_consumption_by_file(file, sheetname=sheetname, process=process)
-    elif file and sheetname:
-        get_consumption_by_file(file, sheetname=sheetname)
+def external(file, sheetname):
+    if file and sheetname:
+        get_consumption_by_file(file, sheetname=sheetname, process=True)
     elif file:
-        get_consumption_by_file(file)
+        get_consumption_by_file(file, process=True)
 
 if __name__ == "__main__":
     try:
