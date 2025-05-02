284a8aac56c46aaa1d1670e16f0dc07a398f63c4
Angxandralol <angyeemarin.dev@gmail.com>
2025-05-02 15:43:59 -0400

    chore: add constants

---
+++ constants/columns.py
@@ -0,0 +1,7 @@
+class NameColumns:
+    """Column names to get the data from the process."""
+
+    BRAS: str = "Bras"
+    STATE: str = "Estado"
+    TOTAL_CLIENTS: str = "Total Clientes"
+    CONSUMPTION: str = "Consumo"
---
+++ constants/sheetnames.py
@@ -0,0 +1,5 @@
+class SheetNames:
+    """Class to get the name of the sheets."""
+    ADSL: str = "Data ADSL"
+    MDU: str = "Data MDU"
+    OLT: str = "Data OLT"
