9925db01fa259b1b689df6d1cd4971781b52eb33
Alexandra <angyeemarin.dev@gmail.com>
2025-05-02 18:57:54 -0400

    test: add unit test

--- test/export_test.py
+++ test/export_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 from libs.export.data import DataExport
 from libs.process.data import DataHandler
@@ -28,25 +29,61 @@ class TestData(unittest.TestCase):
         self.consumption.create_excel()
         return self.consumption.filepath
     
-    def delete_files(self) -> None:
+    def delete_files(self, filepath: str | None = None) -> None:
         """Delete the files."""
         self.asf.delete_excel()
         self.boss.delete_excel()
         self.consumption.delete_excel()
+        if filepath:
+            if os.path.exists(filepath):
+                os.remove(filepath)
 
-    def test_export(self):
-        """Test the export."""
+    def test_export_consumption_by_state_adsl(self):
+        """Test the export consumption by state ADSL."""
         asf_path = self.create_asf()
         boss_path = self.create_boss()
         consumption_path = self.create_consumption()
-        dataHandler = DataHandler(boss_path, consumption_path, asf_path, process_consumption=False)
-        status = DataExport.consumption_by_state(dataHandler)
-        self.delete_files()
+        dataHandler = DataHandler(boss_path, consumption_path, asf_path, process_consumption=True)
+        filepath = DataExport.consumption_adsl_by_state(dataHandler, filepath="./test.xlsx")
+        self.delete_files(filepath="./test.xlsx")
 
-        self.assertTrue(status)
+        self.assertIsNotNone(filepath)
 
+    def test_export_consumption_by_state_mdu(self):
+        """Test the export consumption by state MDU."""
+        asf_path = self.create_asf()
+        boss_path = self.create_boss()
+        consumption_path = self.create_consumption()
+        dataHandler = DataHandler(boss_path, consumption_path, asf_path, process_consumption=True)
+        filepath = DataExport.consumption_mdu_by_state(dataHandler, filepath="./test.xlsx")
+        self.delete_files(filepath="./test.xlsx")
+
+        self.assertIsNotNone(filepath)
+
+    def test_export_consumption_by_state_olt(self):
+        """Test the export consumption by state OLT."""
+        asf_path = self.create_asf()
+        boss_path = self.create_boss()
+        consumption_path = self.create_consumption()
+        dataHandler = DataHandler(boss_path, consumption_path, asf_path, process_consumption=True)
+        filepath = DataExport.consumption_olt_by_state(dataHandler, filepath="./test.xlsx")
+        self.delete_files(filepath="./test.xlsx")
+
+        self.assertIsNotNone(filepath)
+
+    def test_export_consumption_by_state(self):
+        """Test the export consumption by state."""
+        asf_path = self.create_asf()
+        boss_path = self.create_boss()
+        consumption_path = self.create_consumption()
+        dataHandler = DataHandler(boss_path, consumption_path, asf_path, process_consumption=True)
+        filepath = DataExport.consumption_by_state(dataHandler, filepath="./test.xlsx")
+        self.delete_files(filepath="./test.xlsx")
+
+        self.assertIsNotNone(filepath)
 
 
 if __name__ == "__main__":
-    unittest.main()
+    test = TestData()
+    test.test_export()
 
