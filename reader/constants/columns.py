4e3837f55cd976e8766adf71064d527fcccf296a
Alexandra <angyeemarin.dev@gmail.com>
2025-05-02 18:04:48 -0400

    chore: delete unnecessary importation

--- reader/constants/columns.py
+++ reader/constants/columns.py
@@ -1,12 +1,3 @@
-class NameColumns:
-    """Column names to get the data from the process."""
-
-    BRAS: str = "Bras"
-    STATE: str = "Estado"
-    TOTAL_CLIENTS: str = "Total Clientes"
-    CONSUMPTION: str = "Consumo"
-    
-
 class BossNameColumns:
     """Column names to get the data from the process."""
 
