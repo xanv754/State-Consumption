de842a7d77f6d1041e29db330333e1af509460bf
Douglas Santana <jeansf64@gmail.com>
2025-10-16 14:32:43 -0400

    feat: Agregado scraping de SSOMP y configuración relacionada; refactor: Mejoras en consultas de inserción y actualización de base de datos; docs: Renombrado log.py a logger.py; misc: Nuevo archivo de log y ajustes en configuración de entorno y URI

--- state_consumption/database/__main__.py
+++ state_consumption/database/__main__.py
@@ -23,5 +23,22 @@ def destroy(dev: bool = False, testing: bool = False) -> None:
     mongo.drop_collection()
 
 
+@cli.command(help="Ejecuta el scrapper para obtener y guardar los nodos.")
+@click.option("--dev", is_flag=True, help="Usa la base de datos de desarrollo.")
+@click.option("--testing", is_flag=True, help="Usa la base de datos de pruebas.")
+def scrape(dev: bool = False, testing: bool = False) -> None:
+    """
+    Ejecuta el scrapper de SSOMP y guarda los datos en la base de datos.
+    """
+    from state_consumption.database.scrapping.ssomp import SsompScrapper
+    scrapper = SsompScrapper(dev=dev, testing=testing)
+    df = scrapper.run_scrapping()
+
+    if df is not None and not df.empty:
+        scrapper.save_to_database(df)
+    else:
+        print("No se extrajeron datos o se produjo un error.")
+
+
 if __name__ == "__main__":
     cli()
